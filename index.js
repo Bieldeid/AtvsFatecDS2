d3cb19721ec30656518138a6df3fe87ba52d3ae4
Bieldeid <gabrieldeid.android@gmail.com>
2022-09-28 11:27:29 -0300

    adicionado as funções selecet, insert, delete e update no db.js

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.get("/produto", function (req,resp){
     resp.send("Pagina de produto do meu app");
 })
 
+console.log("SELECT * FROM t_usuario");
+const usuarios = await db.selectUsuario({us_nome: "Zé", us_senha: "12345678"});
+console.log(usuarios);
+
+console.log("DELETE FROM t_usuario");
+const result3 = await db.deleteUsuario(2);
+console.log(result30);
+
+console.log('UPDATE USUARIO');
+const result2 = await db.updateUsuario(3, {us_nome: "Zé José", us_senha: "12345678" });
+console.log (result2);
 
 
 //Esse app.listen deve ser a ultima linha sempre (Isso é apenas uma boa prática, não é necessariamente obrigatório)
--- model/service/db.js
+++ model/service/db.js
@@ -1,30 +1,41 @@
 const Connection = require("mysql2/typings/mysql/lib/Connection")
 async function connect(){
-
     //Confirma se está conectado com a variavel global
-
     if(global.connection && global.connection.state !== 'disconnected')
-
         return global.connection;
 
-
-
     const mysql = require ("mysql2/promise");
-
     const connection = await mysql.createConnection ("mysql://root:root@localhost:3306/webii");
-
     console.log("Conectou no MySQL!")
-
     global.connection = connection;
-
     return connection;
-
 }
 
 async function selectUsuario(){
     const conn = await Connection();
-    const [rows] = await Connection.query ('SELECT * FROM usuario');
+    const [rows] = await Connection.query ('SELECT * FROM t_usuario');
     return rows
 }
 
-module.exports={selectUsuario}
+async function insertUsuario(usuario){
+    const conn = await connect();
+    const sql = 'INSERT INTO t_usuario (us_nome, us_senha) VALUES (?,?);';
+    const values = [usuario.node, usuario.senha];
+    return await conn.query(sql, values);
+}
+
+async function deleteUsuario(id){
+    const conn = await connect();
+    const sql = 'DELETE FROM t_usuario where id = ?;';
+    return await conn.query(sql, [id]);
+}
+
+async function updateUsuario(id, usuario){
+    const conn = await connect();
+    const sql= 'UPDATE t_usuario SET us_nome=?, us_senha=? WHERE id_usuario=?';
+    const values = [usuario.nome, usuario.senha, id];
+    return await coon.query(sql, value);
+}
+
+module.exports = {selectUsuario, insertUsuario, deleteUsuario, updateUsuario}
+
